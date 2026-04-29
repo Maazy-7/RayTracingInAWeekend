1e71a43c624272a5696876a2108192d8379fb0a8
Maaz <maazsalman2006@gmail.com>
2026-04-29 00:41:41 -0400

    further developments

--- RayTracingInAWeekend/vec3.h
+++ RayTracingInAWeekend/vec3.h
@@ -3,47 +3,42 @@
 #include <iostream>
 #include <cmath>
 
-class vec3 {
+class vec3 
+{
 public:
-    double e[3];
+    double x, y, z;
 
     vec3() 
-        : e{ 0,0,0 } 
+        : x(0), y(0), z(0)
     {
     
     }
-    vec3(double e) 
-        : e{ e, e, e } 
+    vec3(double v) 
+        : x(v), y(v), z(v)
     {
     
     }
-    vec3(double e0, double e1, double e2) 
-        : e{ e0, e1, e2 } 
+    vec3(double vx, double vy, double vz) 
+        : x(vx), y(vy), z(vz)
     {
     
     }
 
-    double x() const { return e[0]; }
-    double y() const { return e[1]; }
-    double z() const { return e[2]; }
-
-    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-    double operator[](int i) const { return e[i]; }
-    double& operator[](int i) { return e[i]; }
+    vec3 operator-() const { return vec3(-x, -y, -z); }
 
     vec3& operator+=(const vec3& v) 
     {
-        e[0] += v.e[0];
-        e[1] += v.e[1];
-        e[2] += v.e[2];
+        x += v.x;
+        y += v.y;
+        z += v.z;
         return *this;
     }
 
     vec3& operator*=(double t) 
     {
-        e[0] *= t;
-        e[1] *= t;
-        e[2] *= t;
+        x *= t;
+        y *= t;
+        z *= t;
         return *this;
     }
 
@@ -59,7 +54,7 @@ public:
 
     double length_squared() const 
     {
-        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+        return x * x + y * y + z * z;
     }
 };
 
@@ -70,27 +65,27 @@ using point3 = vec3;//alias for vec3
 
 inline std::ostream& operator<<(std::ostream& out, const vec3& v) 
 {
-    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+    return out << v.x << ' ' << v.y << ' ' << v.z;
 }
 
 inline vec3 operator+(const vec3& u, const vec3& v) 
 {
-    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+    return vec3(u.x + v.x, u.y + v.y, u.z + v.z);
 }
 
 inline vec3 operator-(const vec3& u, const vec3& v) 
 {
-    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+    return vec3(u.x - v.x, u.y - v.y, u.z - v.z);
 }
 
 inline vec3 operator*(const vec3& u, const vec3& v) 
 {
-    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+    return vec3(u.x * v.x, u.y * v.y, u.z * v.z);
 }
 
 inline vec3 operator*(double t, const vec3& v) 
 {
-    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+    return vec3(t * v.x, t * v.y, t * v.z);
 }
 
 inline vec3 operator*(const vec3& v, double t) 
@@ -105,16 +100,16 @@ inline vec3 operator/(const vec3& v, double t)
 
 inline double dot(const vec3& u, const vec3& v) 
 {
-    return u.e[0] * v.e[0]
-        + u.e[1] * v.e[1]
-        + u.e[2] * v.e[2];
+    return u.x * v.x
+        + u.y * v.y
+        + u.z * v.z;
 }
 
 inline vec3 cross(const vec3& u, const vec3& v) 
 {
-    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
-        u.e[2] * v.e[0] - u.e[0] * v.e[2],
-        u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+    return vec3(u.y * v.z - u.z * v.y,
+        u.z * v.x - u.x * v.z,
+        u.x * v.y - u.y * v.x);
 }
 
 inline vec3 unit_vector(const vec3& v) 
